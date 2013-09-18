6980375c7982bdb21600a0265a5166ccf1821868
Petri Partanen <petri.j.partanen@metropolia.fi>
2013-09-18 19:35:52 +0300

    Added HelloWorldController

---
+++ HelloASP/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloASP.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "This is default action";
+        }
+
+        public string Welcome(string name, int age)
+        {
+            return "Hello, " + name + ". Your age must be " + age;
+        }
+
+    }
+}
