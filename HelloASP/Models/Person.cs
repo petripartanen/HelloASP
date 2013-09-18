577f83c1ee7b1df94693426ed36f90410b0a548d
Petri Partanen <petri.j.partanen@metropolia.fi>
2013-09-18 23:37:17 +0300

    New Person entity

---
+++ HelloASP/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace HelloASP.Models
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+    }
+
+    public class PersonContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+    }
+}
